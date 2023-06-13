9b08a816d8f0c97b328d9b5a1c383070882bb39d
ostix360 <vicrobin1001@gmail.com>
2023-06-13 14:43:03 +0200

    eprintln & minigrep done => next anonym func

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,28 +1,103 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, env};
 
-pub struct Args<'a>{
+pub struct Config<'a>{
     pub search: &'a String,
-    pub file: &'a String
+    pub file: &'a String,
+    pub sensitive_case: bool
 }
 
-impl Args<'_>{
-    pub fn new(args: &Vec<String>) -> Result<Args, &'static str> {
+impl Config<'_>{
+    pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
         if args.len() != 3{
             return Err("2 arguments are exepted 1: word to search, 2: file to look at")
         }
         let search = &args[1];
         let file = &args[2];
-        Ok(Args{
+
+        let sensitive_case = env::var("MINIGREP_INSENSITIVE_CASE").is_err();
+        Ok(Config {
             search,
-             file
+            file,
+            sensitive_case,
         })
     }
 
 }
 
-pub fn run(args: Args) -> Result<(), Box<dyn Error>>{
-    let content = fs::read_to_string(args.file)?;
+
+pub fn find_case<'a>(search: &str, content: &'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
+
+    for line in content.lines(){
+        if line.contains(search){
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+
+pub fn find_uncased<'a>(search: &str, content: &'a str) -> Vec<&'a str>{
+    let search = search.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in content.lines(){
+        if line.to_lowercase().contains(&search){
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let content = fs::read_to_string(config.file)?;
+
+    let res = if config.sensitive_case{
+        find_case(&config.search, &content)
+    }else{
+        find_uncased(&config.search, &content)
+    };
     
-    println!("Inside this text:\n{}", content);
+    for line in res{
+        println!("{}", line);
+    }
     Ok(())
 }
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn case(){
+        let search = "duct";
+        let content = "\
+Rust:
+security, fasted, productivity.
+Get this 3 elements at the same time.";
+
+        assert_eq!(
+            vec!["security, fasted, productivity."],
+            find_case(search, content)
+        );
+    }
+
+     #[test]
+    fn uncased(){
+        let search = "dUCt";
+        let content = "\
+Rust:
+security, fasted, productivity.
+Get this 3 elements at the same time.";
+
+        assert_eq!(
+            vec!["security, fasted, productivity."],
+            find_uncased(search, content)
+        );
+    }
+}
+
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,19 +1,18 @@
 use std::{env, process};
 
-use minigrep::Args;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let args = Args::new(&args).unwrap_or_else(|err| {
-        println!("Error with arguments:\n{}", err);
+    let args = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Error with arguments:\n{}", err);
         process::exit(1);
     });
-    
-    println!("We are looking for {:?} in {:?}", args.search, args.file);
+
 
     if let Err(e) = minigrep::run(args){
-        println!("Error :\n{}", e);
+        eprintln!("Error :\n{}", e);
 
         process::exit(1);
     }
