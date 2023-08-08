27d9a75e84111dadd440dc7a780bc0b0a49b18aa
ostix360 <vicrobin1001@gmail.com>
2023-08-08 11:51:01 +0200

    update Rc<T> done => next RefCell

--- box_pointer/src/main.rs
+++ box_pointer/src/main.rs
@@ -1,12 +1,21 @@
+use std::rc::Rc;
 use crate::List::{Cons, Nil};
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 fn main() {
     println!("Hello, world!");
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
+    let list = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(
+                Cons(3, Rc::new(Nil)))))));
+    println!("Count after creation of list {}", Rc::strong_count(&list));
+    let a = Cons(0, Rc::clone(&list));
+    println!("Count after creation of a {}", Rc::strong_count(&list));
+    {
+        let b = Cons(-1, Rc::clone(&list));
+        println!("Count after creation of b {}", Rc::strong_count(&list));
+    }
+    println!("Count after b is out of scope {}", Rc::strong_count(&list));
 }
